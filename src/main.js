172b62a30a50dd8b5e3f204fedf80097d69e1a57
benjaminkent <benjamin.k.jehl@gmail.com>
2019-03-31 00:06:13 -0400

    set up axios logic

--- src/main.js
+++ src/main.js
@@ -2,12 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueAxios from 'vue-axios'
+import { securedAxiosInstance, plainAxiosInstance } from './backend/axios'
 import './main.css'
 
 Vue.config.productionTip = false
 
+Vue.use(VueAxios, {
+  secured: securedAxiosInstance,
+  plain: plainAxiosInstance
+})
+
 new Vue({
   router,
   store,
+  securedAxiosInstance,
+  plainAxiosInstance,
   render: h => h(App)
 }).$mount('#app')
